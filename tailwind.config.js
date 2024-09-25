ff4e2211b96dd7f9732967c460963944d209249b
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-25 22:31:05 +0530

    Extend Tailwind configuration with custom gradient like netflix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-custom":
+          "linear-gradient(to right, rgba(33, 13, 22, 1) 16%, rgba(184, 40, 105, 1), rgba(229, 9, 20, 1), rgba(184, 40, 105, 1), rgba(33, 13, 22, 1) 84%)",
+      },
+    },
   },
   plugins: [],
 };
