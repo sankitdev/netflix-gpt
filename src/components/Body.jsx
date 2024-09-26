0d740af3652d5e1920df6edfd7e059099004ac64
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-26 18:30:43 +0530

    Add Observer onAuthSateChag to monitor SignIn and SignOut

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import Login from "./Login";
 import Browse from "./Browse";
 import Home from "./Home";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase.js";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../store/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
   const router = createBrowserRouter([
     {
       path: "/",
@@ -18,6 +25,27 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log("Auth State Changed:", user);
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
   return (
     <div className="min-h-screen bg-black text-white">
       <RouterProvider router={router} />
