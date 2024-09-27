0b0962ca789471538c552e33be765c9a3dee6715
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-27 09:17:35 +0530

    Move Image Constant to utils dir

--- src/utils/Images.js
+++ src/utils/Images.js
@@ -2,3 +2,5 @@ export const Logo =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const bgImg =
   "https://assets.nflxext.com/ffe/siteui/vlv3/47c2bc92-5a2a-4f33-8f91-4314e9e62ef1/web/IN-en-20240916-TRIFECTA-perspective_72df5d07-cf3f-4530-9afd-8f1d92d7f1a8_large.jpg";
+export const ErrorImg =
+  "https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif";
