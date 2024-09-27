de3f7f834b8d2cc17bd25ec792c680d7eddbab52
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-27 22:50:31 +0530

    Move the onAuthState to Header

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,14 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Login from "./Login";
 import Browse from "./Browse";
 import Home from "./Home";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase.js";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../store/userSlice";
+import Error from "./Error";
 
 const Body = () => {
-  const dispatch = useDispatch();
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+      errorElement: <Error />,
     },
     {
       path: "/login",
@@ -25,27 +19,7 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      console.log("Auth State Changed:", user);
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const { uid, email, displayName, photoURL } = user;
-        dispatch(
-          addUser({
-            uid: uid,
-            email: email,
-            displayName: displayName,
-            photoURL: photoURL,
-          })
-        );
-      } else {
-        // User is signed out
-        dispatch(removeUser());
-      }
-    });
-  }, []);
+
   return (
     <div className="min-h-screen bg-black text-white">
       <RouterProvider router={router} />
