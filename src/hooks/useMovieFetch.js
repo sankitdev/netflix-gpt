1be982b6d1c0dd88c144a1f7bbf2e979ef5739c7
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-28 22:06:14 +0530

    Create Custom Hook to manage Fetch and Store Data

---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,24 @@
+import { useCallback, useEffect } from "react";
+import { url, options } from "../utils/Constant";
+import { useDispatch, useSelector } from "react-redux";
+import { popularMovies } from "../store/movieSlice";
+
+const useMovieFetch = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movieData);
+  const movieData = useCallback(async () => {
+    if (movies && movies.length > 0) return;
+    console.log("Test");
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      dispatch(popularMovies(data.results));
+    } catch (error) {
+      console.error("Error while MovieFetch", error);
+    }
+  }, [dispatch, movies]);
+  useEffect(() => {
+    movieData();
+  }, [movieData]);
+};
+export default useMovieFetch;
