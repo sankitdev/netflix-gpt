7cb403a0db93a9ca088652a7ff5b7ac743cb9b5f
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-28 22:09:23 +0530

    Subscribe to movie Store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,4 +1,20 @@
+import Header from "./Header";
+import useMovieFetch from "../hooks/useMovieFetch";
+import { useSelector } from "react-redux";
+import BrowseHero from "./BrowseHero";
+import BrowseRest from "./BrowseRest";
+import Footer from "./Footer";
 const Browse = () => {
-  return <h1>Broser</h1>;
+  useMovieFetch();
+  const movieList = useSelector((store) => store.movieData);
+  console.log(movieList);
+  return (
+    <div className="h-screen">
+      <Header />
+      <BrowseHero />
+      <BrowseRest />
+      <Footer />
+    </div>
+  );
 };
 export default Browse;
