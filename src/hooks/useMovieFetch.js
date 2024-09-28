7c8a208ff040000dfbef4e09a90bb6da59906851
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-28 23:57:25 +0530

    Update the Slice name

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,18 +1,18 @@
 import { useCallback, useEffect } from "react";
 import { url, options } from "../utils/Constant";
 import { useDispatch, useSelector } from "react-redux";
-import { popularMovies } from "../store/movieSlice";
+import { setMoviesList } from "../store/movieSlice";
 
 const useMovieFetch = () => {
   const dispatch = useDispatch();
-  const movies = useSelector((store) => store.movieData);
+  const movies = useSelector((store) => store.movieData.moviesList);
   const movieData = useCallback(async () => {
     if (movies && movies.length > 0) return;
     console.log("Test");
     try {
       const response = await fetch(url, options);
       const data = await response.json();
-      dispatch(popularMovies(data.results));
+      dispatch(setMoviesList(data.results));
     } catch (error) {
       console.error("Error while MovieFetch", error);
     }
