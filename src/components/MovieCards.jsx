ae908c7f680c1d26c13d54f813d9cdeb7b463cb8
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-30 00:17:27 +0530

    feat(ui) : add MovieCard component - Sepearation of Concerns

---
+++ src/components/MovieCards.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import { POSTER_IMG_URL } from "../utils/Constant";
+const MovieCards = ({ movieData, title }) => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold py-5 px-10 ">{title}</h1>
+      <div className="flex gap-5 px-10 overflow-x-auto scrollbar-hide">
+        {movieData.map((items) => (
+          <div key={items.id} className="flex-none">
+            <img
+              src={POSTER_IMG_URL + items.poster_path}
+              alt={items.original_title}
+              className="w-52 h-80 object-cover hover:scale-95 transition-all duration-150 ease-in"
+            />
+            <div className="text-center mt-2">
+              <h1 className="text-lg font-semibold truncate w-52">
+                {items.original_title}
+              </h1>
+              <h2 className="text-sm">
+                Released On -{" "}
+                {new Date(items.release_date).toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+MovieCards.propTypes = {
+  movieData: PropTypes.array,
+  title: PropTypes.string,
+};
+export default MovieCards;
