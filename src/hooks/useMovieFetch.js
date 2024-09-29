875baecee08b8de160d3f145b4acab7c5db4ea2a
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-29 23:51:26 +0530

    feat(movie-fetch): implement multiple movie category fetching
    - Fetch 'now_playing', 'popular', 'top_rated', and 'upcoming' categories
    - Store each category in separate redux state
    - Optimize API calls using Promise.all for concurrent fetching

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,24 +1,66 @@
 import { useCallback, useEffect } from "react";
-import { url, options } from "../utils/Constant";
+import { options } from "../utils/Constant";
 import { useDispatch, useSelector } from "react-redux";
-import { setMoviesList } from "../store/movieSlice";
+import {
+  setNowPlayingList,
+  setPopularMoviesList,
+  setTopRatedList,
+  setUpComingList,
+} from "../store/movieSlice";
 
 const useMovieFetch = () => {
+  const movieUrls = ["now_playing", "popular", "top_rated", "upcoming"];
   const dispatch = useDispatch();
-  const movies = useSelector((store) => store.movieData.moviesList);
+  const movies = useSelector((store) => store.movieData);
   const movieData = useCallback(async () => {
-    if (movies && movies.length > 0) return;
-    console.log("Test");
+    if (
+      movies.nowPlayingList ||
+      movies.popularMoviesList ||
+      movies.topRatedList ||
+      movies.upComingList
+    )
+      return;
+
+    console.log("fetching Data");
+
     try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      dispatch(setMoviesList(data.results));
+      const fetchMovies = movieUrls.map(async (category) => {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`,
+          options
+        );
+        const data = await response.json();
+        return { category, data: data.results };
+      });
+      const results = await Promise.all(fetchMovies);
+      console.log(results);
+      results.forEach(({ category, data }) => {
+        switch (category) {
+          case "now_playing":
+            dispatch(setNowPlayingList(data));
+            break;
+          case "popular":
+            dispatch(setPopularMoviesList(data));
+            break;
+          case "top_rated":
+            dispatch(setTopRatedList(data));
+            break;
+          case "upcoming":
+            dispatch(setUpComingList(data));
+            break;
+
+          default:
+            console.error("Unknown category: ", category);
+        }
+      });
     } catch (error) {
-      console.error("Error while MovieFetch", error);
+      console.error("Something Wrong in MovieFetch hook", error);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, movies]);
   useEffect(() => {
     movieData();
   }, [movieData]);
 };
+
 export default useMovieFetch;
