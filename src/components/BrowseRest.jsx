4a0ee6acdcaf536f94752a28b95033059ed6c039
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-30 00:18:52 +0530

    refactor(movie-fetch): fetching and passing every movie types to MovieCards

--- src/components/BrowseRest.jsx
+++ src/components/BrowseRest.jsx
@@ -1,17 +1,27 @@
-const BrowseRest = () => {
+import PropTypes from "prop-types";
+import MovieCards from "./MovieCards";
+const BrowseRest = ({ movieData }) => {
+  const nowPlaying = movieData.nowPlayingList;
+  const popularList = movieData.popularMoviesList;
+  const topRatedList = movieData.topRatedList;
+  const upComingList = movieData.upComingList;
   return (
-    <div className="h-screen bg-black">
-      <h1>Trending Now</h1>
-      <div className="flex gap-5">
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-      </div>
+    <div className="min-h-screen bg-black">
+      <MovieCards movieData={nowPlaying} title={"Now Playing"} />
+      <MovieCards movieData={popularList} title={"Trending"} />
+      <MovieCards movieData={topRatedList} title={"Top Rated"} />
+      <MovieCards movieData={upComingList} title={"Up Coming"} />
     </div>
   );
+};
+
+BrowseRest.propTypes = {
+  movieData: PropTypes.arrayOf(
+    PropTypes.shape({
+      original_title: PropTypes.string.isRequired,
+      poster_path: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default BrowseRest;
